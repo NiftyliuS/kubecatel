9672497a30d5f87ebc3bcc1b61851735d42a6eac
ratchetgg <vladi.volchkov@gmail.com>
2022-08-01 01:00:48 +0300

    react typescript hot reload

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Routes ,Route, Link } from 'react-router-dom';
+import {Test1,Test2} from './tests/test1';
 
-function render() {
+const App = ()=> (<Router>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to your Electron application.</p>
+    <Link to={'/test1'}>link1</Link>
+    <br />
+    <Link to={'/test2'}>link2</Link>
+    <br />
+    <Routes>
+        <Route path="/test1" element={<Test1/>}/>
+        <Route path="/test2" element={<Test2/>}/>
+    </Routes>
+</Router>)
 
-
-    ReactDOM.render(<>
-        <h2>Hello from React!5</h2>
-
-    </>, document.body);
-}
-
-render();
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/tests/test1.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const Test1=()=> {
+    return (<h1>Edit me (/test1)</h1>)
+}
+
+export const Test2=()=> {
+    return (<h1>Edit me (/test2)</h1>)
+}
