0aea84fc6fe758dfc06f8b334c42e58ef2f6f201
ratchetgg <vladi.volchkov@gmail.com>
2022-08-04 02:11:56 +0300

    Manually imported code from https://github.com/coreui/coreui-free-react-admin-template

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,43 @@
-import * as React from 'react';
+import React, {Component, Suspense} from 'react'
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes ,Route, Link } from 'react-router-dom';
-import {Test1,Test2} from './tests/test1';
+import {HashRouter, Route, Routes} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import store from './store'
+import './scss/style.scss'
 
-const App = ()=> (<Router>
-    <h1>💖 Hello World!</h1>
-    <p>Welcome to your Electron application.</p>
-    <Link to={'/test1'}>link1</Link>
-    <br />
-    <Link to={'/test2'}>link2</Link>
-    <br />
-    <Routes>
-        <Route path="/test1" element={<Test1/>}/>
-        <Route path="/test2" element={<Test2/>}/>
-    </Routes>
-</Router>)
+const loading = (
+    <div className="pt-3 text-center">
+        <div className="sk-spinner sk-spinner-pulse"></div>
+    </div>
+)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Containers
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'))
+const Register = React.lazy(() => import('./views/pages/register/Register'))
+const Page404 = React.lazy(() => import('./views/pages/page404/Page404'))
+const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
+
+class App extends Component {
+    render() {
+
+        return (<Provider store={store}>
+            <HashRouter>
+                <Suspense fallback={loading}>
+                    <Routes>
+                        <Route path="/login" element={<Login/>}/>
+                        <Route path="/register" element={<Register/>}/>
+                        <Route path="/404" element={<Page404/>}/>
+                        <Route path="/500" element={<Page500/>}/>
+                        <Route path="*" element={<DefaultLayout/>}/>*
+                    </Routes>
+                </Suspense>
+            </HashRouter>
+        </Provider>)
+
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
